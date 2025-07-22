357aa19b1619e412d3aced4379e962189cfd7ae9
Ismail Rizwan <ismailrana919@gmail.com>
2025-07-22 16:45:12 +0500

    first commit

---
+++ second.py
@@ -0,0 +1,9 @@
+print("Hello My name is Muhammad Ismail Rana")
+
+print("Gonna print the even numbers")
+for i in range(10):
+    if i % 2 == 0:
+        print(f"Even Number: {i}")
+    else: 
+        print(f"Odd Number: {i}")
+        
